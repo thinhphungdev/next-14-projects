f02fe97fba10cf43a92eaf7ad0de96235afe719f
thinhphungdev <phungthinh0946519356@gmail.com>
2024-03-09 12:43:13 +0700

    initial linking between pages

--- corp-next-app/src/app/page.tsx
+++ corp-next-app/src/app/page.tsx
@@ -1,5 +1,11 @@
-import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <>
+      <Link href='/performance'>Performance</Link>
+      <Link href='/reliability'>Reliability</Link>
+      <Link href='/scale'>Scale</Link>
+    </>
+  );
 }
