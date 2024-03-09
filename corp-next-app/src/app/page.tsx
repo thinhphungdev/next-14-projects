6e8cfc1290fd12744f3fcfdd36b7004a2d52294a
thinhphungdev <phungthinh0946519356@gmail.com>
2024-03-09 13:53:54 +0700

    feat: common UI layout

--- corp-next-app/src/app/page.tsx
+++ corp-next-app/src/app/page.tsx
@@ -1,11 +1,5 @@
 import Link from 'next/link';
 
 export default function Home() {
-  return (
-    <>
-      <Link href='/performance'>Performance</Link>
-      <Link href='/reliability'>Reliability</Link>
-      <Link href='/scale'>Scale</Link>
-    </>
-  );
+  return <>HomePage</>;
 }
---
+++ corp-next-app/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+function Header() {
+  return (
+    <div>
+      <Link href='/'>Home</Link>
+      <Link href='/performance'>Performance</Link>
+      <Link href='/reliability'>Reliability</Link>
+      <Link href='/scale'>Scale</Link>
+    </div>
+  );
+}
+
+export default Header;
