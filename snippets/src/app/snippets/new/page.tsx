e05c4a82d7a6444f0a2df5c91570466ab263c9e1
thinhphungdev <phungthinh0946519356@gmail.com>
2024-03-09 16:18:01 +0700

    create snippet with server action

--- snippets/src/app/snippets/new/page.tsx
+++ snippets/src/app/snippets/new/page.tsx
@@ -1,3 +1,59 @@
+import { db } from '@/db/index';
+import { redirect } from 'next/navigation';
+
 export default function SnippetCreatePage() {
-  return <div>Create a Snippet</div>;
+  async function createSnippet(formData: FormData) {
+    // This need to be a server action
+    'use server';
+
+    // check user input and make sure the're valid
+    const title = formData.get('title') as string;
+    const code = formData.get('code') as string;
+
+    // Create new record in DB
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    console.log(snippet);
+
+    // redirect back to home page
+    redirect('/');
+  }
+
+  return (
+    <form>
+      <h3 className='font-bold m-3'>Create a Snippet</h3>
+      <div className='flex flex-col gap-4'>
+        <div className='flex gap-4'>
+          <label htmlFor='title' className='w-12'>
+            Title
+          </label>
+          <input
+            type='text'
+            name='title'
+            id='title'
+            className='border rounded p-2 w-full'
+          />
+        </div>
+        <div className='flex gap-4'>
+          <label htmlFor='code' className='w-12'>
+            Code
+          </label>
+          <textarea
+            name='code'
+            id='code'
+            className='border rounded p-2 w-full'
+          />
+        </div>
+
+        <button type='submit' className='rounded p-2 bg-blue-200'>
+          Create snippet
+        </button>
+      </div>
+    </form>
+  );
 }
