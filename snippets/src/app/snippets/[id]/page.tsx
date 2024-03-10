538cf17ef565a0f757d939aa630474b29416f26f
thinhphungdev <phungthinh0946519356@gmail.com>
2024-03-10 10:13:42 +0700

    Style the show content of snippet page

--- snippets/src/app/snippets/[id]/page.tsx
+++ snippets/src/app/snippets/[id]/page.tsx
@@ -18,5 +18,20 @@ export default async function SnippetDetailsPage(
 
   if (!snippet) return notFound();
 
-  return <div>{snippet.title}</div>;
+  return (
+    <div>
+      <div className='flex m-4 items-center justify-between'>
+        <h1 className='text-xl font-bold'>{snippet.title}</h1>
+
+        <div className='flex gap-4'>
+          <button className='p-2 border rounded'>Edit</button>
+          <button className='p-2 border rounded'>Delete</button>
+        </div>
+      </div>
+
+      <pre className='p-3 border rounded bg-gray-200 border-gray-200'>
+        <code>{snippet.code}</code>
+      </pre>
+    </div>
+  );
 }
