6851eb74054cae04ac76d317d5a3b6f59cf10864
thinhphungdev <phungthinh0946519356@gmail.com>
2024-03-10 09:54:16 +0700

    add custom 404 page

---
+++ snippets/src/app/snippets/[id]/not-found.tsx
@@ -0,0 +1,9 @@
+export default function SnippetNotFound() {
+  return (
+    <div>
+      <h1 className='text-xl font-bold'>
+        Sorry we could not find this snippet
+      </h1>
+    </div>
+  );
+}
