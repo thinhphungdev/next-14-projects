ce0c8c27efb5f0a5734108df90a56034651e027c
thinhphungdev <phungthinh0946519356@gmail.com>
2024-03-11 11:53:29 +0700

    revalidate path

---
+++ snippets/blog.md
@@ -0,0 +1,10 @@
+# Blog Post Ideas for NextJS Distlled Series
+
+1. Server Actions
+   Server Component and Client Component deep dive
+
+2. Form Validation with UseFormState Hook
+
+3. Next Caching System
+
+4. Authentication with Next-Auth
--- snippets/src/actions/index.ts
+++ snippets/src/actions/index.ts
@@ -2,6 +2,7 @@
 
 import { db } from "@/db";
 import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
 
 export async function editSnippet(id: number, code: string) {
     await db.snippet.update({
@@ -15,7 +16,9 @@ export async function editSnippet(id: number, code: string) {
 export async function deleteSnippet(id: number) {
     await db.snippet.delete({
         where: { id },
-    })
+    });
+
+    revalidatePath('/')
 
     redirect(`/`)
 }
@@ -62,6 +65,8 @@ export async function createSnippet(formState: { message: string }, formData: Fo
             }
         }
     }
+
+    revalidatePath('/')
 
     // redirect back to home page
     redirect('/');
