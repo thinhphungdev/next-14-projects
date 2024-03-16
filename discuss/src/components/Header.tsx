cf2fe5a72d689ba8a33b2f645538bc5ce0012e52
thinhphungdev <phungthinh0946519356@gmail.com>
2024-03-16 14:59:11 +0700

    feat: header auth

--- discuss/src/components/Header.tsx
+++ discuss/src/components/Header.tsx
@@ -5,60 +5,10 @@ import {
   NavbarContent,
   NavbarItem,
   Input,
-  Button,
-  Avatar,
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
 } from '@nextui-org/react';
-
-import { auth } from '@/auth';
-import * as actions from '@/actions';
+import HeaderAuth from './HeaderAuth';
 
 async function Header() {
-  const session = await auth();
-
-  let authContent: React.ReactNode;
-
-  if (session?.user) {
-    authContent = (
-      <Popover placement='left'>
-        <PopoverTrigger>
-          <Avatar src={session.user.image || ''} />
-        </PopoverTrigger>
-        <PopoverContent>
-          <div className='p-4'>
-            <form action={actions.signOut}>
-              <Button type='submit' color='secondary' variant='bordered'>
-                Sign Out
-              </Button>
-            </form>
-          </div>
-        </PopoverContent>
-      </Popover>
-    );
-  } else {
-    authContent = (
-      <>
-        <NavbarItem>
-          <form action={actions.signIn}>
-            <Button type='submit' color='secondary' variant='bordered'>
-              Sign In
-            </Button>
-          </form>
-        </NavbarItem>
-
-        <NavbarItem>
-          <form action={actions.signOut}>
-            <Button type='submit' color='primary' variant='flat'>
-              Sign Out
-            </Button>
-          </form>
-        </NavbarItem>
-      </>
-    );
-  }
-
   return (
     <Navbar className='shadow mb-6'>
       <NavbarBrand>
@@ -71,7 +21,9 @@ async function Header() {
           <Input />
         </NavbarItem>
       </NavbarContent>
-      <NavbarContent justify='end'>{authContent}</NavbarContent>
+      <NavbarContent justify='end'>
+        <HeaderAuth />
+      </NavbarContent>
     </Navbar>
   );
 }
