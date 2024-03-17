ffaaa325783dedab69ddb8ad5ddbafaa12ac98d5
thinhphungdev <phungthinh0946519356@gmail.com>
2024-03-17 13:36:49 +0700

    feat: database form error handling

--- discuss/src/actions/create-topic.ts
+++ discuss/src/actions/create-topic.ts
@@ -2,6 +2,11 @@
 
 import { z } from 'zod';
 import { auth } from '@/auth';
+import type { Topic } from '@prisma/client';
+import { redirect } from 'next/navigation';
+import { db } from "@/db";
+import paths from '@/paths'
+import { revalidatePath } from 'next/cache';
 
 const createTopicSchema = z.object({
     name: z.string().min(3).regex(/^[a-z-]+$/, { message: "Must be lowercase lettersor dashes without spaces" }),
@@ -17,7 +22,8 @@ type CreateTopicFormState = {
 }
 
 export async function createTopic(formState: CreateTopicFormState, formData: FormData): Promise<CreateTopicFormState> {
-    // Todo: revalidate homepage
+
+    // Form Validation stuffs
     const result = createTopicSchema.safeParse({
         name: formData.get('name'),
         description: formData.get('description'),
@@ -30,7 +36,7 @@ export async function createTopic(formState: CreateTopicFormState, formData: For
     }
 
     const session = await auth();
-    
+
     if (!session || !session.user) {
         return {
             errors: {
@@ -39,7 +45,31 @@ export async function createTopic(formState: CreateTopicFormState, formData: For
         }
     }
 
-    return {
-        errors: {}
+    //// Create a new topic in Db
+    let topic: Topic;
+    try {
+        topic = await db.topic.create({
+            data: {
+                slug: result.data.name,
+                description: result.data.description
+            }
+        });
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            return {
+                errors: {
+                    _form: [err.message]
+                }
+            }
+        } else {
+            return {
+                errors: {
+                    _form: ['Something went wrong']
+                }
+            }
+        }
     }
+    // revalidate  the homepage
+    revalidatePath('/');
+    redirect(paths.topicShowPath(topic.slug));
 }
