e68d20c31902ee6885b3dc578d28768c26890688
thinhphungdev <phungthinh0946519356@gmail.com>
2024-03-17 16:47:09 +0700

    create a post form validation

--- discuss/src/actions/create-post.ts
+++ discuss/src/actions/create-post.ts
@@ -1,5 +1,70 @@
 'use server'
 
-export async function createPost() {
- // revalidate the topic show page
+import type { Post } from "@prisma/client"
+import { revalidatePath } from "next/cache"
+import { z } from 'zod';
+import { redirect } from "next/navigation";
+import { db } from "@/db";
+import { auth } from "@/auth"
+
+const createPostSchema = z.object({
+    title: z.string().min(3),
+    content: z.string().min(10)
+})
+
+type CreatePostFormState = {
+    errors: {
+        title?: string[],
+        content?: string[],
+        _form?: string[]
+    }
+}
+
+export async function createPost(formState: CreatePostFormState, formData: FormData): Promise<CreatePostFormState> {
+    // Form fields validation stuffs
+    const result = createPostSchema.safeParse({
+        title: formData.get('title'),
+        content: formData.get('content'),
+    })
+
+    if (result && !result.success) {
+        return {
+            errors: result.error.flatten().fieldErrors,
+        }
+    }
+
+
+    const session = await auth();
+
+    if (!session || !session.user) {
+        return {
+            errors: {
+                _form: ['You must be logged in to do this.']
+            }
+        }
+    }
+
+    let post: Post;
+
+    try {
+        post = await db.post.create({
+            data: {
+                title: result.data.title,
+                content: result.data.content,
+            }
+        })
+    } catch (err) {
+        if (err instanceof Error) {
+            return {
+                errors: {
+                    _form: [err.message]
+                }
+            }
+        }
+    }
+
+    // revalidate the topic show page
+    return {
+        errors: {}
+    }
 }
--- discuss/src/components/posts/post-create-form.tsx
+++ discuss/src/components/posts/post-create-form.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useFormState } from 'react-dom';
 
 import {
@@ -14,13 +16,17 @@ import FormButton from '../common/form-button';
 import * as actions from '@/actions';
 
 function PostCreateForm() {
+  const [formState, action] = useFormState(actions.createPost, {
+    errors: {},
+  });
+
   return (
     <Popover placement='left'>
       <PopoverTrigger>
         <Button color='primary'>Create a post</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action={actions.createPost}>
+        <form action={action}>
           <div className='flex flex-col gap-4 p-4 w-80'>
             <h3 className='text-lg'>Create a post</h3>
             <Input
@@ -28,14 +34,24 @@ function PostCreateForm() {
               label='Title'
               labelPlacement='outside'
               placeholder='Title'
+              isInvalid={!!formState.errors.title}
+              errorMessage={formState.errors.title?.join(', ')}
             />
 
-            <Input
+            <Textarea
               name='content'
               label='content'
               labelPlacement='outside'
               placeholder='Content'
+              isInvalid={!!formState.errors.content}
+              errorMessage={formState.errors.content?.join(', ')}
             />
+
+            {formState.errors?._form ? (
+              <div className='p-2 bg-red-200 border border-red-400'>
+                {formState.errors?._form?.join(', ')}
+              </div>
+            ) : null}
 
             <FormButton>Create</FormButton>
           </div>
