0e7055f4c6731d93a1c3fcf62304b81ff4ac15a4
thinhphungdev <phungthinh0946519356@gmail.com>
2024-03-18 16:15:01 +0700

    feat: post show component

--- discuss/src/app/topics/[slug]/posts/[postId]/page.tsx
+++ discuss/src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,7 +1,29 @@
+import Link from 'next/link';
 import React from 'react';
+import paths from '../../../../../paths';
+import PostShow from '@/components/posts/post-show';
 
-function PostPage() {
-  return <div>PostPage</div>;
+type PostShowPageProps = {
+  params: {
+    slug: string;
+    postId: string;
+  };
+};
+
+function PostShowPage({ params }: PostShowPageProps) {
+  const { postId, slug } = params;
+
+  return (
+    <div className='space-y-3'>
+      <Link
+        className='underline decoration-solid'
+        href={paths.topicShowPath(slug)}
+      >
+        Back to {slug}
+      </Link>
+      <PostShow postId={postId} />
+    </div>
+  );
 }
 
-export default PostPage;
+export default PostShowPage;
